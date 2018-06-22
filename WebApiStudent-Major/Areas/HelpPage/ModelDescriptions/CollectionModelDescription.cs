255b753bda3395f4f03637cac681e4f86ea384c2
michaelbuchanan23 <michaelbuchanan23@gmail.com>
2018-06-22 10:06:36 -0400

    Add project files.

---
+++ WebApiStudent-Major/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace WebApiStudent_Major.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
